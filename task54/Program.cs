03a9801bfd03af4a2d44a14ba6be2f811eb121de
Aleksandr <tabruka@yandex.ru>
2022-11-24 19:59:13 +0300

    Добавил gitignore и задачи №54 и 56

---
+++ task54/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// Объявление двумерного массива
+int[,] array = new int[,] {{1, 4, 7, 2,},
+                            {5, 9, 2, 3,},
+                            {8, 4, 2, 4}};
+
+Console.WriteLine();
+Console.WriteLine("Заданный массив");
+PrintArray(array);
+
+// Цикл для замены
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = 0;
+                temp = array[i, k];
+                array[i, k] = array[i, k + 1];
+                array[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с элементами строки по убыванию");
+PrintArray(array);
+
+
+// Метод вывода массива на экран
+void PrintArray(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
---
+++ task56/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+// Объявление двумерного массива
+int[,] array = new int[,] {{1, 4, 7, 2,},
+                            {5, 9, 2, 3,},
+                            {8, 4, 2, 4},
+                            {5, 2, 6, 7}};
+
+Console.WriteLine();
+Console.WriteLine("Заданный массив");
+PrintArray(array);
+int minSum = Int32.MaxValue;
+int indexRows = 0;
+
+// Цикл поиска наименьшей суммы
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum = sum + array[i, j];        
+    }
+    if (sum < minSum)
+    {
+        minSum = sum;
+        indexRows = indexRows + 1;
+    }
+}
+
+Console.WriteLine($"Строка под номером {(indexRows)} является строкой с наименьшей суммой элементов. Сумма ее элементов равна {(minSum)}");
+
+// Метод вывода массива на экран
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
